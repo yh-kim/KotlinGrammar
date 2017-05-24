f95bba4e612134cdbd4ee1c57275b79403972fed
yh-kim <btc_yh@naver.com>
2017-05-24 21:57:05 +0900

    Observer Pattern(Head First Design Patterns ~ 116p)

---
+++ src/day0524/dp/CurrentConditionsDisplay.kt
@@ -0,0 +1,33 @@
+package day0524.dp
+
+/**
+ * Created by yonghoon on 2017-05-24
+ * Blog   : http://blog.pickth.com
+ * Github : https://github.com/yh-kim
+ * Mail   : yonghoon.kim@pickth.com
+ */
+class CurrentConditionsDisplay(weatherData: Subject): Observer, DisplayElement {
+    var temperature: Float = 0F
+    var humidity: Float = 0F
+    var pressure: Float = 0F
+    var weatherData: Subject? = null
+
+    init {
+        this.weatherData = weatherData
+        weatherData.registerObserver(this)
+    }
+
+
+
+    override fun update(temp: Float, humidity: Float, pressure: Float) {
+        this.temperature = temp
+        this.humidity = humidity
+        this.pressure = pressure
+
+        display()
+    }
+
+    override fun display() {
+        println("Current conditions: $temperature F degrees and $humidity % humidity")
+    }
+}
---
+++ src/day0524/dp/WeatherData.kt
@@ -0,0 +1,43 @@
+package day0524.dp
+
+/**
+ * Created by yonghoon on 2017-05-24
+ * Blog   : http://blog.pickth.com
+ * Github : https://github.com/yh-kim
+ * Mail   : yonghoon.kim@pickth.com
+ */
+class WeatherData: Subject {
+    val observers = mutableListOf<Observer>()
+    var temperature: Float = 0F
+    var humidity: Float = 0F
+    var pressure: Float = 0F
+
+
+    override fun registerObserver(o: Observer) {
+        observers.add(o)
+    }
+
+    override fun removeObserver(o: Observer) {
+        val index = observers.indexOf(o)
+
+        if(index >= 0)
+            observers.removeAt(index)
+    }
+
+    override fun notifyObservers() {
+        for(x in observers)
+            x.update(temperature, humidity, pressure)
+    }
+
+    fun measurementsChanged() {
+        notifyObservers()
+    }
+
+    fun setMeasurements(temperature: Float, humidity: Float, pressure: Float) {
+        this.temperature = temperature
+        this.humidity = humidity
+        this.pressure = pressure
+
+        measurementsChanged()
+    }
+}
