465f2054fbcbdaa95d6441ecdaed671e1c9ba1c9
yh-kim <btc_yh@naver.com>
2017-05-31 23:36:20 +0900

    Solve the BOJ algorithm

---
+++ src/algorithm/boj/Boj11053.kt
@@ -0,0 +1,38 @@
+package algorithm.boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+/**
+ * Created by yonghoon on 2017-05-31
+ * Blog   : http://blog.pickth.com
+ * Github : https://github.com/yh-kim
+ * Mail   : yonghoon.kim@pickth.com
+ *
+ * site   : https://www.acmicpc.net/problem/11053
+ */
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    var length = 0
+    val count = br.readLine().toInt()
+
+    val weightValues = Array(count, {0})
+
+    val valueList = br.readLine()
+            .split(' ')
+            .map { it.trim().toInt() }
+
+    for(i in 0..count - 1) {
+        for(j in 0..i-1) {
+            if(valueList[i] > valueList[j]) {
+                weightValues[i] = if(weightValues[i] > weightValues[j]) weightValues[i] else weightValues[j]
+            }
+        }
+
+        weightValues[i]++
+        length = if(weightValues[i] > length) weightValues[i] else length
+    }
+
+    print(length)
+}
