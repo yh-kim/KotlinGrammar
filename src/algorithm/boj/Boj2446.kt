d1f3f0da89232edd8c61f61b8a538c9364242537
Yonghoon Kim <btc_yh@naver.com>
2017-10-01 19:16:59 +0900

    Solve boj algorithms
    2443, 2444, 2446

---
+++ src/algorithm/boj/Boj2446.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 Yonghoon Kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package algorithm.boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+/**
+ * Created by yonghoon on 2017-10-01
+ * Blog   : http://blog.pickth.com
+ * Site   : https://www.acmicpc.net/problem/2446
+ */
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val N = br.readLine().trim().toInt()
+    br.close()
+
+    for(i in 0 until N) {
+        for(j in 0 until 2*N-i-1) {
+            if(j < i) {
+                print(" ")
+            }else {
+                print("*")
+            }
+        }
+        println()
+    }
+
+    for(i in N-2 downTo 0) {
+        for(j in 0 until 2*N-i-1) {
+            if(j < i) {
+                print(" ")
+            }else {
+                print("*")
+            }
+        }
+        println()
+    }
+}
